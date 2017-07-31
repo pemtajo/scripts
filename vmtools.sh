e2a46febad35d5e9d4f344091312babd79254933
Pedro Matias de Araujo <pmdragon@outlook.com>
2017-07-31 15:48:56 -0300

    Refresh files

--- vmtools.sh
+++ vmtools.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
+#incomplete script for configure vmtools in vmware
 cp -R /media/cdrom/ /tmp/
 cd /tmp/cdrom
-tar zxpf 
+tar zxpf
 
 cd vmware-tools-distrib
 ./vmware-install.pl
 cd
 rm -R /tmp/cdrom
-
