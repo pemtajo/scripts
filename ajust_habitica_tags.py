74afa2685d49f607d08a98cf215f98d31ca6121c
Pedro Matias de Araújo <pedrom.araujo@outlook.com>
2020-07-14 20:13:05 -0300

    normalize dict

--- ajust_habitica_tags.py
+++ ajust_habitica_tags.py
@@ -6,6 +6,7 @@ import os
 import time
 import datetime
 import requests
+import json
 
 BASE_URL = "https://habitica.com/api/v3"
 
@@ -17,7 +18,48 @@ def getHeader():
     }
 
 
-r = requests.get(url=BASE_URL + "/tags", headers=getHeader())
+def getUnicode(tag):
+    sts = tag["name"].split()
+    return sts[2] if len(sts) > 2 else None
 
 
-print(r.json()["data"])
+def getFather(tag, tags):
+    sts = tag["name"].split()
+    number = ".".join(sts[0].split(".")[:-1])
+    for t in tags:
+        number_father = t["name"].split()[0]
+        if number_father == number:
+            return t["id"]
+    else:
+        return None
+
+
+def getTags():
+    r = requests.get(url=BASE_URL + "/tags", headers=getHeader())
+    tags = r.json()["data"]
+    return [
+        {
+            "id": tag["id"],
+            "name": tag["name"],
+            "unicode": getUnicode(tag),
+            "father": getFather(tag, tags),
+        }
+        for tag in tags
+    ]
+
+
+def getTasks():
+    r = requests.get(url=BASE_URL + "/tasks/user", headers=getHeader())
+    tasks = r.json()["data"]
+    return [
+        {"id": task["_id"], "text": task["text"], "tags": task["tags"]}
+        for task in tasks
+        if task["type"] != "reward" and task["type"] != "completedTodos"
+    ]
+
+
+tasks = getTasks()
+tags = getTags()
+
+
+print(json.dumps(tags, indent=3))
